d0bc1c67876b24ad486696bca1cc6ebe4848d944
enrycoop <enrycoop92@gmail.com>
2025-06-30 16:14:50 +0200

    14. Groovy-style builder

---
+++ 01_builder_groovyStyle/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+
+
+
+int main() {
+
+    return 0;
+}
