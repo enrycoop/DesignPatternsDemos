0478e25d5fbd3bc5f876229f586d48bc3c43cd5b
Enrico Coppolecchia <enricocoppolecchia@outlook.com>
2025-07-03 17:40:13 +0200

    fix

--- 02_Factories/main.cpp
+++ 02_Factories/main.cpp
@@ -6,40 +6,32 @@ using namespace std;
 
 class Point
 {
-    class PointFactory;
-    //friend class PointFactory; // OCP
-
+    friend class PointFactory; // OCP
     Point(float x, float y) : x(x), y(y) {}
-    float x, y;
 public:
+    float x, y;
     friend std::ostream& operator<<(std::ostream& os, const Point& obj)
     {
-        return os
-            << "x: " << obj.x
-            << " y: " << obj.y;
+        return os << "x: " << obj.x << " y: " << obj.y;
     }
-    static PointFactory Factory;
-private:
-    class PointFactory
-    {
-    public:
-        static Point NewCartesian(float x, float y)
-        {
-            return {x, y};
-        }
-
-        static Point NewPolar(float r, float theta)
-        {
-            return {r * cos(theta), r * sin(theta)};
-        }
-    };
 };
 
-
+class PointFactory
+{
+public:
+    static Point NewCartesian(float x, float y)
+    {
+        return {x, y};
+    }
+    static Point NewPolar(float r, float theta)
+    {
+        return {r * cos(theta), r * sin(theta)};
+    }
+};
 
 int main()
 {
-    Point p = Point::Factory::NewPolar(5, M_PI_4);
+    Point p = PointFactory::NewPolar(5, M_PI_4);
     cout << p << endl;
     return 0;
 }
