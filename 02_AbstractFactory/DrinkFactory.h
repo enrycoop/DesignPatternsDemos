b69bdb068f07683a81a5634639bf9c0ac16f67b8
enrycoop92 <enrycoop92@gmail.com>
2025-07-05 17:40:21 +0200

    FunctionalFactory

--- 02_AbstractFactory/DrinkFactory.h
+++ 02_AbstractFactory/DrinkFactory.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 #include "HotDrink.h"
 #include "HotDrinkFactory.h"
 #include <map>
@@ -17,5 +19,27 @@ public:
         auto drink = hot_factories[name]->make();
         drink->prepare(200);
         return drink;
+    }
+};
+
+class DrinkWithVolumeFactory {
+    map<string, function<unique_ptr<HotDrink>()>>factories;
+public:
+    DrinkWithVolumeFactory() {
+        factories["tea"] = [] {
+            auto tea = make_unique<Tea>();
+            tea->prepare(200);
+            return tea;
+        };
+
+        factories["tea"] = [] {
+            auto tea = make_unique<Tea>();
+            tea->prepare(50);
+            return tea;
+        };
+    }
+
+    unique_ptr<HotDrink> make_drink(const string& name) {
+        return factories[name]();
     }
 };
