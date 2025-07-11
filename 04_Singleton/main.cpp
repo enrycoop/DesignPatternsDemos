c6b7ea6b436e601f80de5744e283d2bff794ec00
Enrico Coppolecchia <enricocoppolecchia@outlook.com>
2025-07-11 12:34:49 +0200

    init

--- 04_Singleton/main.cpp
+++ 04_Singleton/main.cpp
@@ -1,7 +1,47 @@
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+#include <boost/lexical_cast.hpp>
+
+class SingletonDatabase
+{
+    SingletonDatabase()
+    {
+        cout << "Initialzing database\n";
+        ifstream ifs("capitals.txt");
+        string s, s2;
+        while(std::getline(ifs, s))
+        {
+            getline(ifs, s2);
+            int pop = lexical_cast<int>(s2);
+            capitals[s] = pop;
+        }
+    }
+    map<string, int> capitals;
+public:
+    //cancelliamo il costruttore
+    SingletonDatabase(SingletonDatabase const&) = delete;
+    SingletonDatabase& operator=(SingletonDatabase const&) = delete;
+
+    static SingletonDatabase& get()
+    {
+        static SingletonDatabase instance;
+        return instance;
+    }
+
+    int get_population(const string& s)
+    {
+        return capitals[s];
+    }
+};
 
 int main()
 {
+    string city = "Tokyo";
+    cout << city << " has population " << SingletonDatabase::get().get_population(city) << endl;
 
     return 0;
 }
