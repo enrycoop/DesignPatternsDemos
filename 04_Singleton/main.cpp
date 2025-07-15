29200107e1dce2fc5301c9acc2fd9ec1aadf9a37
Enrico Coppolecchia <enricocoppolecchia@outlook.com>
2025-07-15 09:28:22 +0200

    refactoring

--- 04_Singleton/main.cpp
+++ 04_Singleton/main.cpp
@@ -5,22 +5,27 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-using namespace std;
-
 #include <boost/lexical_cast.hpp>
 
 
-#define PATH "/home/enrico/CLionProjects/DesignPatternsDemos/04_Singleton/capitals.txt"
+#define PATH "./capitals.txt"
 
 
-class SingletonDatabase
+class Database
+{
+    public:
+    virtual ~Database() = default;
+    virtual int get_population(const std::string& name) = 0;
+};
+
+class SingletonDatabase final : public Database
 {
     SingletonDatabase()
     {
-        cout << "Initialzing database\n";
-        ifstream ifs(PATH);
+        std::cout << "Initialzing database\n";
+        std::ifstream ifs(PATH);
         if (ifs.is_open()) {
-            string s, s2;
+            std::string s, s2;
             while(std::getline(ifs, s))
             {
                 getline(ifs, s2);
@@ -28,12 +33,12 @@ class SingletonDatabase
                 capitals[s] = pop;
             }
             if (capitals.empty())
-                cout << "No capitals found\n";
+                std::cout << "No capitals found\n";
         }else {
-            cout << "Unable to open database file\n";
+            std::cout << "Unable to open database file\n";
         }
     }
-    map<string, int> capitals;
+    std::map<std::string, int> capitals;
 public:
     //cancelliamo il costruttore
     SingletonDatabase(SingletonDatabase const&) = delete;
@@ -45,14 +50,31 @@ public:
         return instance;
     }
 
-    int get_population(const string& s)
+    int get_population(const std::string& s) override
+    {
+        return capitals[s];
+    }
+};
+
+class DummyDatabase : public Database
+{
+    std::map<std::string, int> capitals;
+    public:
+    DummyDatabase()
+    {
+        capitals["alpha"] = 1;
+        capitals["beta"] = 2;
+        capitals["charlie"] = 3;
+    }
+
+    int get_population(const std::string& s) override
     {
         return capitals[s];
     }
 };
 
 struct SingletonRecordFinder {
-    int total_population(vector<string> names) {
+    int total_population(const std::vector<std::string>& names) {
         int result{0};
         for (auto& name : names) {
             result += SingletonDatabase::get().get_population(name);
@@ -61,17 +83,43 @@ struct SingletonRecordFinder {
     }
 };
 
+struct ConfigurableRecordFinder
+{
+    Database& db;
+
+    explicit ConfigurableRecordFinder(Database& db) : db(db) {}
+
+    int total_population(std::vector<std::string> names) {
+        int result{0};
+        for (auto& name : names) {
+            result += db.get_population(name);
+        }
+        return result;
+    }
+};
+
+
+//#######################################################//
+// TESTS
+//#######################################################//
+
 TEST(RecordFinderTests, SingletonTotalPopulationTest) {
     SingletonRecordFinder finder;
-    vector<string> names{"Tokyo","Seul"};
+    std::vector<std::string> names{"Tokyo","Seul"};
     int tp = finder.total_population(names);
     ASSERT_EQ(tp, 13960000+9776000);
 }
+
+TEST(RecordFinderTests, DependantTotalPopulationTest) {
+    DummyDatabase db;
+    ConfigurableRecordFinder finder(db);
+    EXPECT_EQ(3, finder.total_population(std::vector<std::string>{"alpha","beta"}));
+}
+
+
 
 int main(int ac, char* av[])
 {
     testing::InitGoogleTest(&ac, av);
     return RUN_ALL_TESTS();
-
-    return 0;
 }
