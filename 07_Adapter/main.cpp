217873d764dea349b6eb8499b9a85a9f961caf33
enrycoop <enrycoop92@gmail.com>
2025-07-18 23:56:51 +0200

    fltk

--- 07_Adapter/main.cpp
+++ 07_Adapter/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <memory>
-
 #include "Geometry.h"
 
 using namespace std;
 
-int main()
-{
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
+int main(int argc, char **argv) {
+    const auto window = make_unique<Fl_Window>(300, 200, "Hello, FLTK");
+    const auto box = make_unique<Fl_Box>(20, 80, 260, 40, "Hello, World!");
+    box->box(FL_UP_BOX);
+    box->labelfont(FL_BOLD + FL_COURIER);
+    box->labelsize(24);
+    window->end();
+    window->show(argc, argv);
+
     vector<shared_ptr<VectorObject>> vectorObjects{
         std::make_shared<VectorRectangle>(10, 10, 100, 100),
         std::make_shared<VectorRectangle>(30, 30, 60, 60),
     };
-
-    return 0;
+    return Fl::run();
 }
+
+
+
+
+
