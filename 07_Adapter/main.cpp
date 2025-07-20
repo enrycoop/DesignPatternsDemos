bd87d38d3a8b680dd850e6ce76afcabb16a7fc7b
enrycoop <enrycoop92@gmail.com>
2025-07-20 20:43:10 +0200

    feat(fltk): used fluid to build GUI

--- 07_Adapter/main.cpp
+++ 07_Adapter/main.cpp
@@ -7,21 +7,51 @@ using namespace std;
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/fl_draw.H>
+
+#include "graphics_gui.h"
+
+//
+// class DrawingWidget : public Fl_Box {
+//     vector<shared_ptr<VectorObject>> objects;
+//
+// public:
+//     DrawingWidget(int x, int y, int w, int h) : Fl_Box(x, y, w, h) {}
+//
+//     void addObject(shared_ptr<VectorObject> obj) {
+//         objects.push_back(obj);
+//     }
+//
+//     void draw() override {
+//         Fl_Box::draw();
+//
+//         fl_color(FL_BLACK);
+//
+//         // Disegna tutti gli oggetti vettoriali
+//         for (const auto& obj : objects) {
+//             for (auto it = obj->begin(); it != obj->end(); ++it) {
+//                 const Line& line = *it;
+//                 fl_line(line.start.x, line.start.y, line.end.x, line.end.y);
+//             }
+//         }
+//     }
+// };
+
 
 int main(int argc, char **argv) {
-    const auto window = make_unique<Fl_Window>(300, 200, "Hello, FLTK");
-    const auto box = make_unique<Fl_Box>(20, 80, 260, 40, "Hello, World!");
-    box->box(FL_UP_BOX);
-    box->labelfont(FL_BOLD + FL_COURIER);
-    box->labelsize(24);
-    window->end();
-    window->show(argc, argv);
+    // const auto window = make_unique<Fl_Window>(1080, 720, "Hello, FLTK");
+    // const auto drawingArea = make_unique<DrawingWidget>(0, 0, 1080, 720);
+    //
+    // // Aggiungi gli oggetti geometrici al widget di disegno
+    // drawingArea->addObject(std::make_shared<VectorRectangle>(10, 10, 100, 100));
+    // drawingArea->addObject(std::make_shared<VectorRectangle>(30, 30, 60, 60));
+    //
+    // window->end();
+    // window->show();
 
-    vector<shared_ptr<VectorObject>> vectorObjects{
-        std::make_shared<VectorRectangle>(10, 10, 100, 100),
-        std::make_shared<VectorRectangle>(30, 30, 60, 60),
-    };
+    make_window()->show();
     return Fl::run();
+
 }
 
 
